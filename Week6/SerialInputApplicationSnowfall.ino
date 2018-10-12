fae4feb95534e32b63bc49e2b6b046a3c8e36a5a
evapphlips <42977311+evapphlips@users.noreply.github.com>
2018-10-12 12:13:08 -0400

    Create SerialInputApplicationSnowfall.ino

---
+++ Week6/SerialInputApplicationSnowfall.ino
@@ -0,0 +1,13 @@
+// Eva Philips
+// Pcomp Week 6, Serial Input to P5.js Application: Snowfall
+
+void setup() {
+  Serial. begin(9600); // initialize serial communcations
+}
+
+void loop() {
+  int potentiometer = analogRead(A0);  // read the input pin
+  int mappedPot = map(potentiometer, 0, 1023, 0,255 ); // map the potentiometer to fit in 1 byte (aka 0-255)
+  Serial.write(mappedPot);  // print the mapped value to the serial port
+  delay(1); // slight delay to stablize the ADC
+}
